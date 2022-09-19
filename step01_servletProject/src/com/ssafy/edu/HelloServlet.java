72edb10b4260104c8078864c84d7ce4235c6062f
ssooh-i <615ksh@gmail.com>
2022-09-20 03:38:12 +0900

    .

---
+++ step01_servletProject/src/com/ssafy/edu/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.ssafy.edu;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1.한글처리(UTF-8, EUC-KR등)
+		response.setContentType("text/html;charset=UTF-8");
+				
+		//2.출력
+		response.getWriter().append("<html><head><title></title></head>");
+		response.getWriter().append("<body>");
+		response.getWriter().append("헬로우 서블릿<br>");
+		response.getWriter().append("<a href='next.do'>다음으로</a>");
+		response.getWriter().append("</body></html>");
+	}
+}
